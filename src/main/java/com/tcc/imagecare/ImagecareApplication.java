b130aea64496a605202195bfd14eb5e1588fe5f4
Wallace Goularte <wallace.goularte@hotmail.com>
2023-10-05 19:17:05 -0300

    Criação do Projeto

---
+++ src/main/java/com/tcc/imagecare/ImagecareApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.imagecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagecareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagecareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tcc/imagecare/ImagecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.imagecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagecareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
