fce54eafacac2f8cbaf262bb09b071a8c89865a3
Wallace Goularte <wallace.goularte@hotmail.com>
2023-10-15 21:13:15 -0300

    Ajustes

--- src/main/java/com/tcc/imagecare/controllers/AuthenticationController.java
+++ src/main/java/com/tcc/imagecare/controllers/AuthenticationController.java
@@ -5,15 +5,17 @@ import com.tcc.imagecare.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/login")
 public class AuthenticationController {
 
     @Autowired
     private UsuarioService service;
 
-    @PostMapping("/login")
+    @PostMapping
     public boolean checkLogin(@RequestBody UsuarioDTO usuarioDTO) {
         return service.autenticar(usuarioDTO);
     }
